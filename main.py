0fac9228009efa624a697f884dfc01d2519f8418
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-04-21 11:00:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def refact(n):
+def Recurse(n):
     if n==0:
         return 1
     else:
-        p = n * refact(n-1)
+        p = n * Rescurse(n-1)
     
     return p
 
 num = int(input("Enter an integer: "))
-fact = refact(num)
+fact = Rescurse(num)
 print(f'Factorial Value for {num} is {fact}.')
